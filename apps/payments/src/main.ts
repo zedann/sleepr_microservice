d8ca838c36b6673a14402517381e925fa2f268ad
zedan <zedanmohamedcp@gmail.com>
2025-06-10 21:34:52 +0300

    feat: start payments service

---
+++ apps/payments/src/main.ts
@@ -0,0 +1,8 @@
+import { NestFactory } from '@nestjs/core';
+import { PaymentsModule } from './payments.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(PaymentsModule);
+  await app.listen(process.env.port ?? 3000);
+}
+bootstrap();
